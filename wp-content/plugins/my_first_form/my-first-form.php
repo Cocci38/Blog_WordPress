7e5d51085314600590b71505e82efdc7f3d31379
Sandra <sandra.villot038@gmail.com>
2022-06-01 14:57:53 +0200

    plugin s'affiche et fonction dans bonne page

--- wp-content/plugins/my_first_form/my-first-form.php
+++ wp-content/plugins/my_first_form/my-first-form.php
@@ -32,41 +32,56 @@ function formulaire(){
     $dupe = $wpdb->get_results( $wpdb->prepare("SELECT name_champ, type_input, name_label, category_id FROM `wp_mfp_champ`
     INNER JOIN wp_mfp_liaison ON wp_mfp_champ.id_champ = wp_mfp_liaison.champ_id
     INNER JOIN wp_mfp_input ON wp_mfp_liaison.input_id = wp_mfp_input.id_input
-    WHERE category_id = " . $cat[0]->term_taxonomy_id));
-    
-?>
-<form action="" method="post">
-    <?php foreach ($dupe as $key) { 
-        // echo "<pre>",print_r( $key) ,"</pre><br>";?>
-        <input type="hidden" name="<?= 'category_id' ?>" value="<?= $key->category_id ?>">
-        
-        <label for="<?= $key->name_input ?>"><?= $key->name_label ?></label>
-        <input type="<?= $key->name_input ?>" name="<?= $key->name_champ ?>">
-        
-    <?php } ?>
-        <div class="wp-block-button is-style-primary">
-            <button type="submit" class="wp-block-button__link">Je valide</button>
-        </div>
-        
-</form>
-<?php
+    WHERE category_id = " . $cat[0]->term_id));
+
+    if (!empty($_POST)) {
+        $category_id = $_POST['category_id'];
+        $name = $_POST['name'];
+        $comment = $_POST['comment'];
+        $recette = $_POST['recette'];
+        $wpdb->insert(
+        'wp_mfp_formulaire',
+            array(
+                'category_id' => $category_id,
+                'name' => $name,
+                'comment' => $comment,
+                'recette' => $recette,
+            ),array('%d', '%s', '%s','%s' )
+        );
+    }
+
+
+    $result = "<form action='' method='post'><input type='hidden' name= 'category_id' value="
+    . $cat[0]->term_id.">";
+        foreach ($dupe as $key) { 
+            // echo "<pre>",print_r( $key) ,"</pre><br>"
+            $result .= "<label for='$key->name_champ'> $key->name_label </label>
+            <input type='$key->type_input' name='$key->name_champ'>";
+            
+        } 
+        if (!empty($dupe)) {
+            $result .="<div class='wp-block-button is-style-primary'>
+            <button type='submit' class='wp-block-button__link'>Je valide</button>
+        </div>";
+        }
+    "</form>";
+    return $result  ;
 
 //var_dump($_POST);
 //echo "<pre>",print_r($_POST) ,"</pre><br>";die();
-$category_id = $_POST['category_id'];
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$recette = $_POST['recette'];
-$wpdb->insert(
-    'wp_mfp_formulaire',
-        array(
-            'category_id' => $category_id,
-            'name' => $name,
-            'comment' => $comment,
-            'recette' => $recette,
-        ),array('%d', '%s', '%s','%s' )
-);
+
 }
 
-//add_filter('the_content', 'formulaire');
+function afficher($content){
+    $afficher = $content;
+    $afficher .= formulaire();
+    return $afficher;
+}
+
+// function category(){
+//     $result = get_the_category();
+//   print_r($result);
+// }
+
+add_filter('the_content', 'afficher');
 add_action('get_footer', 'formulaire');
